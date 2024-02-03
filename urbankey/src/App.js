683f7c8219dd036fc66cf02a5cd4297d30812fc1
Fadoua Doghmane <doghmanefadoua@gmail.com>
2024-02-03 13:33:46 -0500

    Login Page
    Added Login Page

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SignUp from './Components/Authentication/SignUp.js';
+import Login from './Components/Authentication/Login.js';
 
 function App() {
   return (
     <div className="App">
-    
+      <Login/>
     </div>
   );
 }
