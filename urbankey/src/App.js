2b696270f6a5cdccc0a2e467f88ca5d743bdbea0
Fadoua Doghmane <doghmanefadoua@gmail.com>
2024-02-04 14:27:28 -0500

    add route for profile page

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Authentication/Login.js";
 import SignUp from "./Components/SignUp/SignUp.js";
+import Profile from "./Components/UserProfile/Profile.js";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </Router>
   );
