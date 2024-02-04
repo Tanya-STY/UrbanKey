a43edba5e239e1b722c58a6d206be9ce70b134b9
Fadoua Doghmane <doghmanefadoua@gmail.com>
2024-02-04 12:20:46 -0500

    Updated Login css

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Login from './Components/Authentication/Login.js';
-import Profile from "./Components/Authentication/Profile.js";
+import SignUp from './Components/SignUp/SignUp.js';
 
 function App() {
   return (
     <div className="App">
-      {/*<Login/>*/}
-        <Profile/>
+        <Login/>
     </div>
   );
 }
