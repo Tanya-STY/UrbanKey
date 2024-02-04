3db94d838bce3f9ce810a31add42fdfe26627089
aloulidi <loulidiasmae@gmail.com>
2024-02-04 02:04:39 -0500

    profile page almost done, some modifications to do

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './Components/Authentication/Login.js';
+import Profile from "./Components/Authentication/Profile.js";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/*<Login/>*/}
+        <Profile/>
     </div>
   );
 }
