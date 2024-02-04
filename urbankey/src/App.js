8d7744365c2641bcde6fc687a773534e1364ed42
Fadoua Doghmane <doghmanefadoua@gmail.com>
2024-02-04 15:16:03 -0500

    Added Routes

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import Login from './Components/Authentication/Login.js';
-import SignUp from "./Pages/SignUp/SignUp";
-import Profile from "./Components/UserProfile/Profile";
+// App.js
+
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Authentication/Login.js";
+import SignUp from "./Components/SignUp/SignUp.js";
+import Profile from "./Components/UserProfile/Profile.js";
 
 function App() {
   return (
-    <div className="App">
-      <Profile/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
--- urbankey/src/Components/Authentication/Login.js
+++ urbankey/src/Components/Authentication/Login.js
@@ -3,28 +3,31 @@
 import React from 'react';
 import './Login.css';
 import condoimage from '../Images/Condo.jpg';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
+    <div className="body-attributes">
     <div className="login-container">
       <div className="image-container">
         <img src= {condoimage} alt= "Condo Image"/>
       </div>
-      <div className="form-container">
-        <h2>Login</h2>
-        <div className="form-group">
-          <label>Email:</label>
-          <input type="email" placeholder="Enter your email" />
+      <div className="login-section-container">
+        <h2 className="h2-Login" >Login</h2>
+        <div className="form-group-inputs">
+          <label className="label-email">Email:</label>
+          <input className="input-email" type="email" placeholder="Enter your email" />
         </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input type="password" placeholder="Enter your password" />
+        <div className="form-group-inputs">
+          <label className="label-password">Password:</label>
+          <input className="input-password" type="password" placeholder="Enter your password" />
         </div>
         <button className="login-btn">LOGIN</button>
         <div className="signup-link">
-          Still not a member? <a href="#">Sign Up Now!</a>
+        Still not a member? <Link to="/SignUp">Sign Up Now!</Link>
         </div>
       </div>
+    </div>
     </div>
   );
 };
