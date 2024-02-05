ec1f97c2def9c234c20117c80f499ea56662ab00
Tanya So Tin Yan <tanya.sty99@gmail.com>
2024-02-04 22:38:27 -0500

    Updated Sign Up Page

--- urbankey/src/Components/SignUp/SignUp.js
+++ urbankey/src/Components/SignUp/SignUp.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './SignUp.css';
 import { Link } from 'react-router-dom';
+import urbankeyLogo from '../Images/urbankey_logo.png';
+import { FaLocationDot, FaHouseCircleCheck, FaMedal, FaPenRuler } from "react-icons/fa6";
+
 
 const SignUp = () => {
     const [activeButton, setActiveButton] = useState('Individual');
@@ -52,8 +55,32 @@ const SignUp = () => {
         <div className="outer-container">
             <div className="container">
                 <div className="left-div">
-                    Left Div Content
-                </div>
+                    <div className="container-images-signup" style={{ justifyContent: 'center' }}>
+                        <div className="row-signup"><img src={urbankeyLogo} alt="UrbanKey Logo" /></div>
+                        <div className="row-signup">Why should I joing UrbanKey?</div>
+                        <div className="row-signup" style={{ textAlign: 'center', width: '500px', color: 'darkgray' }}>To have the experience of "property", "information" and "trust" in the condo management world all together...</div>
+                        <div className="row-signup">
+                            <div className="column-signup" style={{ textAlign: 'center' }}>
+                                <div className="column-signup-icons" style={{backgroundColor: '#ffd166'}}><FaLocationDot style={{height: '100px'}}/></div>
+                                <div className="column-signup-icon">Learn about location.</div>
+                            </div>
+                            <div className="column-signup" style={{ textAlign: 'center' }}>
+                                <div className="column-signup-icons" style={{backgroundColor: '#e85d04'}}><FaHouseCircleCheck style={{height: '100px'}}/></div>
+                                <div className="column-signup-icon">Find out the value of your house free of charge.</div>
+                            </div>
+                        </div>
+                        <div className="row-signup">
+                            <div className="column-signup" style={{ textAlign: 'center' }}>
+                                <div className="column-signup-icons" style={{backgroundColor: '#7161ef'}}><FaMedal style={{height: '100px'}}/></div>
+                                <div className="column-signup-icon">Discover houses that will improve your life quality.</div>
+                            </div>
+                            <div className="column-signup" style={{ textAlign: 'center' }}>
+                                <div className="column-signup-icons" style={{backgroundColor: '#00a6fb'}}><FaPenRuler style={{height: '100px'}}/></div>
+                                <div className="column-signup-icon">Be aware of new projects.</div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
 
                 <div className="right-div">
                     <div className="membership-buttons">
@@ -62,8 +89,8 @@ const SignUp = () => {
                     </div>
 
                     <div className="underline-wrapper">
-                        <div className="membership-buttons-underline" style={{ backgroundColor: activeButton === 'Individual' ? 'blue' : 'white' }}></div>
-                        <div className="membership-buttons-underline" style={{ backgroundColor: activeButton === 'Corporate' ? 'blue' : 'white' }}></div>
+                        <div className="membership-buttons-underline" style={{ backgroundColor: activeButton === 'Individual' ? 'blue' : 'lightgrey' }}></div>
+                        <div className="membership-buttons-underline" style={{ backgroundColor: activeButton === 'Corporate' ? 'blue' : 'lightgrey' }}></div>
                     </div>
 
 
@@ -76,9 +103,11 @@ const SignUp = () => {
                     </div>
                     {activeButton === 'Individual' && (
                         <form onSubmit={handleSubmit} className="membership-form">
-                            <div className="form-group">
+                            <div className='user-input-signup'>
+                            <div className="user-form-input">
                                 <label htmlFor="fullName"></label>
                                 <input
+                                    className="user-input"
                                     type="text"
                                     id="fullName"
                                     value={fullName}
@@ -87,9 +116,10 @@ const SignUp = () => {
                                     required
                                 />
                             </div>
-                            <div className="form-group">
+                            <div className="user-form-input">
                                 <label htmlFor="email"></label>
                                 <input
+                                    className="user-input"
                                     type="email"
                                     id="email"
                                     value={email}
@@ -99,9 +129,10 @@ const SignUp = () => {
                                 />
                                 {emailError && <div className="error">{emailError}</div>}
                             </div>
-                            <div className="form-group">
+                            <div className="user-form-input">
                                 <label htmlFor="password"></label>
                                 <input
+                                    className="user-input"
                                     type="password"
                                     id="password"
                                     value={password}
@@ -110,9 +141,10 @@ const SignUp = () => {
                                     required
                                 />
                             </div>
-                            <div className="form-group">
+                            <div className="user-form-input">
                                 <label htmlFor="passwordConfirm"></label>
                                 <input
+                                    className="user-input"
                                     type="password"
                                     id="passwordConfirm"
                                     value={passwordConfirm}
@@ -122,7 +154,9 @@ const SignUp = () => {
                                 />
                                 {passwordError && <div className="error">{passwordError}</div>}
                             </div>
-                            <div className="form-group">
+
+                            </div>
+                            <div className="user-form-input">
                                 <label>
                                     <input
                                         type="checkbox"
@@ -134,7 +168,7 @@ const SignUp = () => {
                                     </span>
                                 </label>
                             </div>
-                            <div className="form-group">
+                            <div className="user-form-input">
                                 <label>
                                     <input
                                         type="checkbox"
@@ -142,88 +176,98 @@ const SignUp = () => {
                                         onChange={(e) => setAllowContact(e.target.checked)}
                                     />
                                     <span onClick={() => setAllowContact(!allowContact)}>
-                                        I allow UrbanKey to contact me via e-mail, SMS, etc for marketing and promotional purposes.
+                                        I allow UrbanKey to contact me for marketing and promotional purposes.
                                     </span>
                                 </label>
+                                <button type="submit" className='SignUp-button'>Sign Up</button>
+
                             </div>
-                            <button type="submit" className='SignUp-button'>Sign Up</button>
+
                         </form>
                     )}
 
                     {activeButton === 'Corporate' && (
                         <form onSubmit={handleSubmit} className="membership-form">
-                            <div className="form-group">
-                                <label htmlFor="fullName"></label>
+                        <div className='user-input-signup'>
+                        <div className="user-form-input">
+                            <label htmlFor="fullName"></label>
+                            <input
+                                className="user-input"
+                                type="text"
+                                id="fullName"
+                                value={fullName}
+                                onChange={(e) => setFullName(e.target.value)}
+                                placeholder='Full name'
+                                required
+                            />
+                        </div>
+                        <div className="user-form-input">
+                            <label htmlFor="email"></label>
+                            <input
+                                className="user-input"
+                                type="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder='Email'
+                                required
+                            />
+                            {emailError && <div className="error">{emailError}</div>}
+                        </div>
+                        <div className="user-form-input">
+                            <label htmlFor="password"></label>
+                            <input
+                                className="user-input"
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder='Password'
+                                required
+                            />
+                        </div>
+                        <div className="user-form-input">
+                            <label htmlFor="passwordConfirm"></label>
+                            <input
+                                className="user-input"
+                                type="password"
+                                id="passwordConfirm"
+                                value={passwordConfirm}
+                                onChange={(e) => setPasswordConfirm(e.target.value)}
+                                placeholder='Retype password'
+                                required
+                            />
+                            {passwordError && <div className="error">{passwordError}</div>}
+                        </div>
+
+                        </div>
+                        <div className="user-form-input">
+                            <label>
                                 <input
-                                    type="text"
-                                    id="fullName"
-                                    value={fullName}
-                                    onChange={(e) => setFullName(e.target.value)}
-                                    placeholder='Full name'
-                                    required
+                                    type="checkbox"
+                                    checked={agreeTerms}
+                                    onChange={(e) => setAgreeTerms(e.target.checked)}
                                 />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="email"></label>
+                                <span style={{ color: 'blue', cursor: 'pointer' }} onClick={() => setAgreeTerms(!agreeTerms)}>
+                                    I agree to the Membership Terms
+                                </span>
+                            </label>
+                        </div>
+                        <div className="user-form-input">
+                            <label>
                                 <input
-                                    type="email"
-                                    id="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    placeholder='Email'
-                                    required
+                                    type="checkbox"
+                                    checked={allowContact}
+                                    onChange={(e) => setAllowContact(e.target.checked)}
                                 />
-                                {emailError && <div className="error">{emailError}</div>}
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password"></label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    placeholder='Password'
-                                    required
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="passwordConfirm"></label>
-                                <input
-                                    type="password"
-                                    id="passwordConfirm"
-                                    value={passwordConfirm}
-                                    onChange={(e) => setPasswordConfirm(e.target.value)}
-                                    placeholder='Retype password'
-                                    required
-                                />
-                                {passwordError && <div className="error">{passwordError}</div>}
-                            </div>
-                            <div className="form-group">
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        checked={agreeTerms}
-                                        onChange={(e) => setAgreeTerms(e.target.checked)}
-                                    />
-                                    <span style={{ color: 'blue', cursor: 'pointer' }} onClick={() => setAgreeTerms(!agreeTerms)}>
-                                        I agree to the Membership Terms
-                                    </span>
-                                </label>
-                            </div>
-                            <div className="form-group">
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        checked={allowContact}
-                                        onChange={(e) => setAllowContact(e.target.checked)}
-                                    />
-                                    <span onClick={() => setAllowContact(!allowContact)}>
-                                        I allow UrbanKey to contact me via e-mail, SMS, etc for marketing and promotional purposes.
-                                    </span>
-                                </label>
-                            </div>
+                                <span onClick={() => setAllowContact(!allowContact)}>
+                                    I allow UrbanKey to contact me for marketing and promotional purposes.
+                                </span>
+                            </label>
                             <button type="submit" className='SignUp-button'>Sign Up</button>
-                        </form>
+
+                        </div>
+                    </form>
                     )}
                     <div className="login-link">
                         Already a member? <Link to="/Login">Login Now!</Link>
