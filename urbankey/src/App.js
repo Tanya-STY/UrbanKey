c816bfb07e7233d8a58a9b7a6abce23482402ba8
mzmddi <110358757+mzmddi@users.noreply.github.com>
2024-02-09 21:08:04 -0500

    put app.js from shamma into the main

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -5,16 +5,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Authentication/Login.js";
 import SignUp from "./Components/SignUp/SignUp.js";
 import Profile from "./Components/UserProfile/Profile.js";
+import { AuthProvider } from "./Provider/AuthProvider.js";
 
 function App() {
   return (
-    <Router>
+    <div>
+    <AuthProvider>
       <Routes>
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Profile" element={<Profile />} />
       </Routes>
-    </Router>
+      </AuthProvider>
+     </div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/Login" element={<Login />} />
+    //     <Route path="/SignUp" element={<SignUp />} />
+    //     <Route path="/Profile" element={<Profile />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
