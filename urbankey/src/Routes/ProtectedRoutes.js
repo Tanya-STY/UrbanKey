4a48d6eae139efad33ad55b8f1acadd5ca2c1604
mzmddi <110358757+mzmddi@users.noreply.github.com>
2024-02-09 21:08:44 -0500

    added two new files in provider from shamma->main

---
+++ urbankey/src/Routes/ProtectedRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigate, Outlet } from "react-router-dom";
+import { useAuth } from "../Provider/AuthProvider";
+
+export const ProtectedRoute = () => {
+    const { token } = useAuth();
+    const navigate = useNavigate();
+
+    if (!token) {
+        navigate('/Login');
+        return null;
+    }
+
+    // if authenticated, render the child routes
+    return <Outlet />
+
+}
