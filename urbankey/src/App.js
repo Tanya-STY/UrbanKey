2b4bb6a839feb6cf6fd21bc2fe46d89b48b5f81f
sonalipxtel <sonalipatel06@hotmail.com>
2024-02-18 19:57:58 -0500

    add Home and Registration Key page front-end

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Authentication/Login.js";
 import SignUp from "./Components/SignUp/SignUp.js";
 import Profile from "./Components/UserProfile/Profile.js";
+import RegistrationKey from "./Components/RegistrationKey/RegistrationKey.js";
+import Home from "./Components/Home/Home.js";
 import { AuthProvider } from "./Provider/AuthProvider.js";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Profile" element={<Profile />} />
+        <Route path="/RegistrationKey" element={<RegistrationKey />} />
+        <Route path="/Home" element={<Home />} /> 
       </Routes>
       </AuthProvider>
      </div>
