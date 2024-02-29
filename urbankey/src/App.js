3b68e32d1234d39941d61f0dbb6ae0bda686c6e5
Fadoua Doghmane <doghmanefadoua@gmail.com>
2024-02-29 14:50:16 -0500

    Nav Bars
    3 navigations bars:
    - homepage nav bar
    - user nav bar
    - company nav bar

--- urbankey/src/App.js
+++ urbankey/src/App.js
@@ -6,20 +6,26 @@ import Login from "./Components/Authentication/Login.js";
 import SignUp from "./Components/SignUp/SignUp.js";
 import Profile from "./Components/UserProfile/Profile.js";
 import Finance from "./Components/Finance/Finance.js";
+import NavBar_HomePage from "./Components/NavBar/NavBar_HomePage";
+import NavBar_Company from "./Components/NavBar/NavBar_Company.js";
+import NavBar_User from "./Components/NavBar/NavBar_User.js";
 import { AuthProvider } from "./Provider/AuthProvider.js";
 
 function App() {
   return (
     <div>
-    <AuthProvider>
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Finance" element={<Finance />} />
-      </Routes>
+      <AuthProvider>
+        
+          <NavBar_HomePage />
+            <Routes>
+              <Route path="/Login" element={<Login />} />
+              <Route path="/SignUp" element={<SignUp />} />
+              <Route path="/Profile" element={<Profile />} />
+              <Route path="/Finance" element={<Finance />} />
+            </Routes>
+        
       </AuthProvider>
-     </div>
+    </div>
     // <Router>
     //   <Routes>
     //     <Route path="/Login" element={<Login />} />
