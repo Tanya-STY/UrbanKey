e61d1cedad1c47c5e971a90ef7f8f1c8f615eaf4
shamma13 <shammamarkis@hotmail.com>
2024-03-16 13:23:09 -0400

    deleted unneeded files

--- backend/role_list.py
+++ backend/role_list.py
@@ -1,6 +1,6 @@
 ROLES_LIST = {
   'User': 2001,
-  'Owner': 1984,
-  'Renter': 3333,
+  'Owner': 3333,
+  'Renter': 1984,
   'Admin': 5150
 }
--- backend/services/bcrypt_services.py
+++
@@ -1,9 +0,0 @@
-
-
-def hash_password(password):
-    """Hashes the given password using bcrypt."""
-    return bcrypt.generate_password_hash(password).decode('utf-8')
-
-def check_password(password, hashed_password):
-    """Checks if the given password matches the hashed password."""
-    return bcrypt.check_password_hash(hashed_password, password)
